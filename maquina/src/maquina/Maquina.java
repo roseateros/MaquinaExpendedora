c80085b7441c4d700248aee34725c91eacf77eaf
Your Name <you@example.com>
2018-11-09 17:30:49 +0100

    desde netbeans

---
+++ maquina/src/maquina/Maquina.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maquina;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Tarde
+ */
+public class Maquina extends JFrame implements ActionListener{
+
+    /**
+     * @param args the command line arguments
+     */
+    public Maquina Action
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
